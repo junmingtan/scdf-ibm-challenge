d31cccb84d93b8ddd001322a260de93a4966d8ad
Tan Jun Ming <junming1996@hotmail.com>
2020-06-14 15:56:31 +0800

    stage2 updates

--- client_dashboard/src/Dashboard.js
+++ client_dashboard/src/Dashboard.js
@@ -142,7 +142,7 @@ export default function Dashboard() {
             <MenuIcon />
           </IconButton>
           <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            5G.11
+            REmergency
           </Typography>
           <CustomPopover/>
         </Toolbar>
